c32876e6ed79d99956c6b91cf81799091159e8c1
petroodetp <241575347+petroodetp@users.noreply.github.com>
2026-09-25 23:12:38 +0200

    fix windows socket conflict

--- src/Gui/Gui.hpp
+++ src/Gui/Gui.hpp
@@ -1,4 +1,7 @@
 #pragma once
+
+#define WIN32_LEAN_AND_MEAN
+
 #include <Includes/Includes.hpp>
 
 using namespace std;
@@ -13,8 +16,5 @@ namespace Gui {
         ImGuiWindowFlags_NoBackground |
         ImGuiWindowFlags_NoScrollbar |
         ImGuiWindowFlags_NoScrollWithMouse;
-
-    inline bool IsAuthenticated = false;
-    inline bool AuthInProgress = false;
 
 }
